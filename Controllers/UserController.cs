7414f4cb70f044982dc2038ea9fcf2e3d8e98294
raj vekariya <raj.r.vekariya@gmail.com>
2020-06-18 20:51:06 +0530

    Add project files.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UserManagement.Models;
+using UserManagement.Service;
+
+namespace UserManagement.Controllers
+{
+	public class UserController : Controller
+	{
+
+		// GET: User
+		public async Task<ActionResult> IndexAsync()
+		{
+			UserService userService = new UserService();
+			var UserList = await userService.GetTblUsersAsync();
+			return View(UserList);
+		}
+
+		// GET: User/Details/5
+		public ActionResult Details(int id)
+		{
+			return View();
+		}
+
+		// GET: User/Create
+		public ActionResult Create()
+		{
+			return View();
+		}
+
+		// POST: User/Create
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<ActionResult> CreateAsync(IFormCollection collection)
+		{
+			try
+			{
+				TblUser user = new TblUser();
+				user.Name = collection["Name"];
+				user.Email = collection["Email"];
+				user.DateOfBirth = Convert.ToDateTime(collection["DateOfBirth"]);
+				user.Address = collection["Address"];
+				UserService userService = new UserService();
+				var result = await userService.CreateNewUser(user);
+
+				if (result)
+					return RedirectToAction(nameof(IndexAsync));
+				else
+					return View();
+			}
+			catch
+			{
+				return View();
+			}
+		}
+
+		// GET: User/Edit/5
+		public async Task<ActionResult> EditAsync(int id)
+		{
+			UserService userService = new UserService();
+			var user = await userService.GetUser(id);
+			return View(user);
+		}
+
+		// POST: User/Edit/5
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<ActionResult> EditAsync(int id, IFormCollection collection)
+		{
+			try
+			{
+				TblUser user = new TblUser();
+				user.Id = id;
+				user.Name = collection["Name"];
+				user.Email = collection["Email"];
+				user.DateOfBirth = Convert.ToDateTime(collection["DateOfBirth"]);
+				user.Address = collection["Address"];
+				UserService userService = new UserService();
+				var result = await userService.UpdateUser(user);
+				// TODO: Add update logic here
+
+				return RedirectToAction(nameof(IndexAsync));
+			}
+			catch
+			{
+				return View();
+			}
+		}
+
+		// GET: User/Delete/5
+		public async Task<ActionResult> Delete(int id)
+		{
+			UserService userService = new UserService();
+			var user = await userService.GetUser(id);
+			return View(user);
+		}
+
+		// POST: User/Delete/5
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<ActionResult> Delete(int id, IFormCollection collection)
+		{
+			try
+			{
+				UserService userService = new UserService();
+				var user = await userService.DeleteUser(id);
+				return RedirectToAction(nameof(IndexAsync));
+			}
+			catch
+			{
+				return View();
+			}
+		}
+	}
+}
---
+++ Service/UserService.cs
@@ -0,0 +1,119 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using UserManagement.Models;
+
+namespace UserManagement.Service
+{
+	public class UserService
+	{
+		string Baseurl = "https://localhost:44377/api/User/";
+		public async Task<List<TblUser>> GetTblUsersAsync()
+		{
+			List<TblUser> UserList = new List<TblUser>();
+
+			using (var client = new HttpClient())
+			{
+				client.BaseAddress = new Uri(Baseurl);
+				client.DefaultRequestHeaders.Clear();
+				client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+				HttpResponseMessage Res = await client.GetAsync("GetUsers");
+
+				if (Res.IsSuccessStatusCode)
+				{
+					var UserResponse = Res.Content.ReadAsStringAsync().Result;
+					UserList = JsonConvert.DeserializeObject<List<TblUser>>(UserResponse);
+				}
+			}
+			return UserList;
+		}
+
+		public async Task<TblUser> GetUser(int Id)
+		{
+			var result = new TblUser();
+
+			using (var client = new HttpClient())
+			{
+				client.BaseAddress = new Uri(Baseurl);
+				client.DefaultRequestHeaders.Clear();
+
+				HttpResponseMessage Res = await client.GetAsync("GetUser?userID=" + Id);
+
+				if (Res.IsSuccessStatusCode)
+				{
+					var UserResponse = Res.Content.ReadAsStringAsync().Result;
+					result = JsonConvert.DeserializeObject<TblUser>(UserResponse);
+				}
+			}
+			return result;
+		}
+
+		public async Task<bool> CreateNewUser(TblUser user)
+		{
+			var result = false;
+
+			using (var client = new HttpClient())
+			{
+				client.BaseAddress = new Uri(Baseurl);
+				client.DefaultRequestHeaders.Clear();
+
+				HttpContent content = new StringContent(JsonConvert.SerializeObject(user), Encoding.UTF8, "application/json");
+
+				HttpResponseMessage Res = await client.PostAsync("AddUser", content);
+
+				if (Res.IsSuccessStatusCode)
+				{
+					result = true;
+				}
+			}
+			return result;
+		}
+
+		public async Task<bool> UpdateUser(TblUser user)
+		{
+			var result = false;
+
+			using (var client = new HttpClient())
+			{
+				client.BaseAddress = new Uri(Baseurl);
+				client.DefaultRequestHeaders.Clear();
+
+				HttpContent content = new StringContent(JsonConvert.SerializeObject(user), Encoding.UTF8, "application/json");
+
+				HttpResponseMessage Res = await client.PostAsync("UpdateUser", content);
+
+				if (Res.IsSuccessStatusCode)
+				{
+					result = true;
+				}
+			}
+			return result;
+		}
+
+		public async Task<bool> DeleteUser(int Id)
+		{
+			var result = false;
+
+			using (var client = new HttpClient())
+			{
+				client.BaseAddress = new Uri(Baseurl);
+				client.DefaultRequestHeaders.Clear();
+
+				HttpContent content = new StringContent(JsonConvert.SerializeObject(Id), Encoding.UTF8, "application/json");
+
+				HttpResponseMessage Res = await client.PostAsync("DeleteUser", content);
+
+				if (Res.IsSuccessStatusCode)
+				{
+					result = true;
+				}
+			}
+			return result;
+		}
+	}
+}
